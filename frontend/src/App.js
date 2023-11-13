9c05181dfb761f4c76c96469407c93eeed731b41
nluizsoliveira <nluizsoliveira@gmail.com>
2023-11-13 06:26:44 -0300

    feat: implement app->navbar state managing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,31 @@
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Component';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home'); // Default page
+
+  const changePage = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <div>Home Page Content</div>;
+      // Add cases for other pages
+      default:
+        return <div>Page Not Found</div>;
+    }
+  };
+
   return (
     <div>
-    <h1>App</h1>
-    <Navbar/ >
+      <h1>App</h1>
+      <Navbar currentPage={currentPage} changePage={changePage} />
+      <div>
+        {renderPage()}
+      </div>
     </div>
   );
 }
--- frontend/src/Components/Navbar/Component.js
+++ frontend/src/Components/Navbar/Component.js
@@ -1,32 +1,59 @@
-import './Style.css'
-import carrinho from './assets/carrinho.png'
-import logo from './assets/logo.png'
-import login from './assets/login.png'
+import React from 'react';
+import './Style.css';
+import carrinho from './assets/carrinho.png';
+import logo from './assets/logo.png';
+import login from './assets/login.png';
 
-function Navbar() {
+function Navbar({ currentPage, changePage }) {
     return (
-        <nav id = 'headerNav'>
-        <img id = 'logo' src={logo} alt="logo"/>
-        <div id = "inputPageListWrapper">
-            <input/>
-            <button>Procurar</button>
-            <ul id = "pageList">
-                <li className = "selected"><a href="home.html">Oferta do Dia</a></li>
-                <li><a href="perifericos.html">Periféricos</a></li>
-                <li><a href="hardware.html">Hardware</a></li>
-                <li><a href="software.html">Software</a></li>
-                <li><a href="monte.html">Monte o Seu</a></li>
-                <li><a href="about.html">Quem Somos</a></li>
-                <li><a href="suporte.html">Suporte</a></li>
-            </ul>
-        </div>
-        <div id = 'iconsWrapper'>
-           <div className = 'iconWrapper'><a href="carrinho.html"><img src={carrinho} alt="carrinho"/><span>Carrinho</span></a></div>
-           <div className = 'iconWrapper'><a href="login.html"><img src={login} alt="entrar"/><span>Login</span></a></div>
-           <div className = 'iconWrapper'><a href="cadastro.html"><img src={login} alt="cadastro"/><span>Cadastro</span></a></div>
-        </div>
-    </nav>
+        <nav id='headerNav'>
+            <img id='logo' src={logo} alt="logo" />
+            <div id="inputPageListWrapper">
+                <input />
+                <button>Procurar</button>
+                <ul id="pageList">
+                    <li className={currentPage === 'home' ? 'selected' : ''}>
+                        <button onClick={() => changePage('home')}>Oferta do Dia</button>
+                    </li>
+                    <li className={currentPage === 'perifericos' ? 'selected' : ''}>
+                        <button onClick={() => changePage('perifericos')}>Periféricos</button>
+                    </li>
+                    <li className={currentPage === 'hardware' ? 'selected' : ''}>
+                        <button onClick={() => changePage('hardware')}>Hardware</button>
+                    </li>
+                    <li className={currentPage === 'software' ? 'selected' : ''}>
+                        <button onClick={() => changePage('software')}>Software</button>
+                    </li>
+                    <li className={currentPage === 'monte' ? 'selected' : ''}>
+                        <button onClick={() => changePage('monte')}>Monte o Seu</button>
+                    </li>
+                    <li className={currentPage === 'about' ? 'selected' : ''}>
+                        <button onClick={() => changePage('about')}>Quem Somos</button>
+                    </li>
+                    <li className={currentPage === 'suporte' ? 'selected' : ''}>
+                        <button onClick={() => changePage('suporte')}>Suporte</button>
+                    </li>
+                </ul>
+            </div>
+            <div id='iconsWrapper'>
+                <div className='iconWrapper'>
+                    <button onClick={() => changePage('carrinho')}>
+                        <img src={carrinho} alt="carrinho" /><span>Carrinho</span>
+                    </button>
+                </div>
+                <div className='iconWrapper'>
+                    <button onClick={() => changePage('login')}>
+                        <img src={login} alt="entrar" /><span>Login</span>
+                    </button>
+                </div>
+                <div className='iconWrapper'>
+                    <button onClick={() => changePage('cadastro')}>
+                        <img src={login} alt="cadastro" /><span>Cadastro</span>
+                    </button>
+                </div>
+            </div>
+        </nav>
     );
-  }
-  
-  export default Navbar;
+}
+
+export default Navbar;
