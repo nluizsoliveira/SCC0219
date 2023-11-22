6dac2c54fc8ed16c629cff84dc468f99a0b386c0
nluizsoliveira <nluizsoliveira@gmail.com>
2023-11-22 15:12:17 -0300

    feat: add and style cart items list

--- frontend/src/Components/Carrinho/Component.js
+++ frontend/src/Components/Carrinho/Component.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './Style.css';
 
-function Carrinho({ cartItems }) {
+function Carrinho({ cartItems, removeFromCart }) {
+    const totalPrice = cartItems.reduce((sum, item) => sum + item.price, 0);
+
     return (
-        <div>
-            <h2>Carrinho - Em Construção</h2>
-            <ul>
+        <div className='carrinho'>
+            <h2>Carrinho</h2>
+            <div className='total-price'>Total: R$ {totalPrice.toFixed(2)}</div>
+            <ul className='cart-list'>
                 {cartItems.map((item, index) => (
-                    <li key={index}>{item.name} - R$ {item.price}</li>
+                    <li key={index} className='cart-item'>
+                        <img src={item.image} alt={item.name} className='cart-item-image' />
+                        <span className='cart-item-name'>{item.name}</span>
+                        <span className='cart-item-price'>R$ {item.price.toFixed(2)}</span>
+                        
+                    </li>
                 ))}
             </ul>
         </div>
